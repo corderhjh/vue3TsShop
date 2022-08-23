2064ad3441fdfcb80ece4d7a7500f39dde9f985d
expressHjh <913389492@qq.com>
2022-08-23 17:14:59 +0800

    hjh

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,32 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+
+
+import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-});
+
+
+
+
+    HelloWorld
+
+
+  }
+})
 </script>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica,
+
+  Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
+
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
