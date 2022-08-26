6730fb7c385a9c7a120bb13f9283b64ae5c12d72
expressHjh <913389492@qq.com>
2022-08-26 10:06:39 +0800

    feat(t): t

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App)
+.use(router)
+.use(store)
+.mount('#app')
 
-console.log('环境', process);
+console.log('环境', process)
